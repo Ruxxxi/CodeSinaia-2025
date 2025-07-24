e56023789b0258240cf1207a49860c7edb04b3d1
Andreea Istrate <istrateandreearuxandra@gmail.com>
2025-07-24 10:34:24 +0300

    okk

--- Chatbot/Console chatbot/probability.py
+++ Chatbot/Console chatbot/probability.py
@@ -55,14 +55,21 @@ RULES = [
 
 def message_probability(user_message, keywords, single_response=False, required=[]):
 
-    #TODO: Calculează probabilitatea mesajului message_certainty
+    # Calculează probabilitatea mesajului message_certainty
+    message_certainty = 0
+    for word in user_message:
+        if word in keywords:
+            message_certainty += 1
     #pt fiecare cuvant din mesaj care apare in recognised_words
     #message_certainty este incrementat
-    
-    #TODO: Calculează match_ratio ca raportul dintre message_certainty și numărul de cuvinte din keywords
+
+    #Calculăm match_ratio ca raportul dintre message_certainty și numărul de cuvinte din keywords
     #dacă keywords este gol, setăm match_ratio la 0
-    
-    
+    if len(keywords) == 0:
+        match_ratio = 0
+    else:
+        match_ratio = message_certainty / len(keywords)
+
     if required:
         if not all(word in user_message for word in required):
             return 0
@@ -76,25 +83,29 @@ def check_all_messages(message):
     best_response = None
 
     for rule in RULES:
-        None
+        #None
         
-        #TODO: Calculează probabilitatea mesajului pentru fiecare regulă
+        # Calculează probabilitatea mesajului pentru fiecare regulă
         #folosind funcția message_probability definită mai sus
-        
-        #TODO: dacă prob este mai mare decât highest_prob,
+        prob = message_probability(message, rule['keywords'], rule.get('single_response', False), rule.get('required', []))
+        #dacă prob este mai mare decât highest_prob,
         # actualizează best_response și highest_prob
-        
-        
-    #TODO: returneaza raspunsul, fie cel de eroare, fie cel gasit 
-    
+        if prob > highest_prob:
+            highest_prob = prob
+            best_response = rule['response']
+    # returneaza raspunsul, fie cel de eroare, fie cel gasit 
+    return best_response if best_response else unknown()
 
 def get_response(user_input):
-    None
-    #TODO: Verifică dacă user_input este gol sau conține doar spații
-
-    #TODO: apeleaza functia split pentru a împărți mesajul în cuvinte 
-    
+    #str1 = "Ana"
+    #return str1
+    # Verifică dacă user_input este gol sau conține doar spații
+    if not user_input or user_input.strip() == "":
+        return "Please say something!"
+    # apeleaza functia split pentru a împărți mesajul în cuvinte 
+    clean_message = re.split(r'\s+|[,;?.-]\s*', user_input.lower())
     # apoi returneaza rezultatul obtinut folosind check_all_messages pentru a verifica mesajul
+    return check_all_messages(clean_message)
 
 # Ce inseamna \s+|[,;?.-]\s*?
 # \s+ înseamnă unul sau mai multe spații albe (inclusiv tab-uri și linii noi)
